139737a1c6cf104f10ec402c4be40e05f6fb9787
takeyato <taketake1568@gmail.com>
2025-09-02 23:39:36 +0900

    Program.cs の更新

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@ using System.Windows.Forms;
 static class Program
 {
     [STAThread]
-        static void Main()
-            {
-                    Application.EnableVisualStyles();
-                            Application.SetCompatibleTextRenderingDefault(false);
-                                    Application.Run(new OverlayForm());
-                                        }
-                                        }
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new OverlayForm());
+    }
+}
